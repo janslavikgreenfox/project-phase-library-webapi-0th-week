2213d6b03d9a2837cdd29408ca756aa8d32f2a98
Jan Slavik <janslavikgreenfox@gmail.com>
2020-11-25 11:37:13 +0100

    Await-Async-Lectures - add the materials

---
+++ Async/Async/Async/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Async
+{
+    class Program
+    {
+        public static async Task Main()
+        {
+            var watch = Stopwatch.StartNew();       
+
+            Console.WriteLine(watch.ElapsedMilliseconds + " Hello from main ");
+            var messageTask = CreateMessage();
+            Task.Run(() => Calculate(watch));
+            await CallMethod(watch);
+           
+            Console.WriteLine(watch.ElapsedMilliseconds + " Hello from main after Tasks");
+            while (!messageTask.IsCompleted)
+            {
+                Console.WriteLine("Message Task is not completed just yet.. let´s give it a few moments");
+                Thread.Sleep(3000);
+            }
+
+            Console.WriteLine(watch.ElapsedMilliseconds + " Message Task was completed");
+
+            watch.Stop();
+            Console.ReadLine();         
+        }
+
+        static async Task CallMethod(Stopwatch watch)
+        {
+            string filePath = "lol.txt";
+
+            Task<int> task = ReadFile(filePath, watch);
+
+            Console.WriteLine(watch.ElapsedMilliseconds + "  Callmethod after task");
+            Console.WriteLine(" Other Work 1");
+            Console.WriteLine(" Other Work 2");
+            Console.WriteLine(" Other Work 3");
+            
+            int length = await task;
+            Console.WriteLine(watch.ElapsedMilliseconds + " Total length of file: " + length);
+            Console.WriteLine(" After work 1");
+            Console.WriteLine(" After work 2");
+        }
+
+        static async Task<int> ReadFile(string file, Stopwatch watch)
+        {
+            Console.WriteLine(watch.ElapsedMilliseconds + " File reading is starting");
+
+            using StreamReader reader = new StreamReader(file);
+            string s = await reader.ReadToEndAsync();
+
+            Console.WriteLine(watch.ElapsedMilliseconds + " File reading is completed");
+
+            int length = s.Length;
+            return length;
+        }
+
+        static void Calculate(Stopwatch watch)
+        {
+            var list = new List<long>();
+            for (long i = 0; i < 999999999; i++)
+            {
+                list.Add(i * 50 - 347 * 3);
+            }
+
+            Console.WriteLine(watch.ElapsedMilliseconds + " FINISHED CALCULATING");
+        }
+
+        static async Task<string[]> CreateMessage()
+        {
+            var list = new List<Task<string>>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                list.Add(Task.Run(() => CreateString()));
+            }
+                var results = await Task.WhenAll(list);
+                return results;
+        }
+
+        static string CreateString() 
+        {
+            var result = String.Empty;
+            for (int i = 0; i < 99999; i++)
+            {
+                result += "Hi";
+            }
+            return result;
+        }
+    }
+}
---
+++ Async/AsyncApp/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsyncApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static void PrintMenu()
+            {
+               Console.WriteLine("Hello there, try to ask for tasks and while you wait, ask for more." + Environment.NewLine +
+              "Options you can pick from:\t" + "\n1. Create a terribly big List and print its count  " + "\n2. Calc fibonacci of nTh element\n"
+                                 );
+            };
+            Task<int> task = null;
+            while (true)
+            {
+                PrintMenu();
+                bool isSuccess = Int32.TryParse(Console.ReadLine(), out int choice);
+                if (isSuccess)
+                {
+                    switch (choice)
+                    {
+                        case 1:
+                        default:
+                            Task.Run(() => CreateListAndSum());
+                            break;
+                        case 2:
+                            task = Task.Run(() => CalculateFib(40));
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Wrong input, go again");
+                }
+
+                Console.WriteLine("\nNow the stuff is calculating, but you can go with another run and ask for input while calculating... Let´s start again");
+                if (task is not null && task.IsCompleted)
+                {
+                    Console.WriteLine("\n" + task.Result);
+                }
+            }
+        }
+
+        private static int CalculateFib(int number)
+        {
+            if (number == 0 || number == 1)
+                return number;
+
+            return CalculateFib(number - 2) + CalculateFib(number - 1);
+        }
+
+        private static void CreateListAndSum()
+        {
+            Console.WriteLine("Starting list creation\n");
+            var list = new List<long>();
+            for (int i = 1; i < 99999999; i++)
+            {
+                list.Add(i * 50);
+            }
+            Console.WriteLine("\n\nList creation finished. The sum of the list is " + list.Sum() + "\n\n");
+        }
+    }
+}
